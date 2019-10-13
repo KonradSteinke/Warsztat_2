8bd3743ab49e78642274c0aa308271e9aa5b6347
Konrad Steinke <steinke.konrad@gmail.com>
2019-10-13 15:25:18 +0200

    E1

---
+++ src/pl/coderslab/DAO/DBUtil.java
@@ -0,0 +1,15 @@
+package pl.coderslab.DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBUtil {
+    private final static String URL =
+            "jdbc:mysql://localhost:3306/Warsztat_2?useSSL=false&characterEncoding=utf8&serverTimzone=UTC";
+    private final static String USER = "root";
+    private final static String PASSWORD = "ThinkPadT580";
+    public static Connection connect() throws SQLException {
+        return DriverManager.getConnection(URL,USER,PASSWORD);
+    }
+}
